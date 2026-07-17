4510bd536fd0d77642fff2acfa007cf10d9b2293
silasjul <silaskierstein@gmail.com>
2026-07-17 15:33:54 +0200

    start

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Scene from '@/components/Scene'
+import LevaControls from '@/components/LevaControls'
+
+export default function Home() {
+  return (
+    <main className="w-full h-dvh">
+      <LevaControls />
+      <Scene />
+    </main>
+  )
+}
