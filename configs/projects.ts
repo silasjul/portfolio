c564494f326ec755c096c3366f74a6e6ec099c00
silasjul <silaskierstein@gmail.com>
2026-07-22 21:44:03 +0200

    bottleship

--- configs/projects.ts
+++ configs/projects.ts
@@ -16,6 +16,7 @@ export const PROJECTS: Project[] = [
   { image: '/audio-viz.png', url: 'https://disco.silaskierstein.com/' },
   { image: '/cloth.png', url: 'https://cloth.silaskierstein.com/' },
   { image: '/ascii.png', url: 'https://ascii.silaskierstein.com/' },
+  { image: '/bottleship.png', url: 'https://bottleship.silaskierstein.com/' },
 ]
 
 export const IMAGES = PROJECTS.map((p) => p.image)
